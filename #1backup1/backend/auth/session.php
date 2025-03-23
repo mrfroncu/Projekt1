32c6518c38943347bd65b923def0e4db0d45e720
mrfroncu <henashd@gmail.com>
2025-03-23 21:30:39 +0100

    pelna funkcjonalnosc zalacznikow

--- #1backup1/backend/auth/session.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once "../config.php";
-
-if (isset($_SESSION['user'])) {
-    echo json_encode(["authenticated" => true, "user" => $_SESSION['user']]);
-} else {
-    echo json_encode(["authenticated" => false]);
-}
-?>
--- #2backup2/backend/auth/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once "../config.php";
-
-if (isset($_SESSION['user'])) {
-    $pdo->prepare("UPDATE users SET is_online = 0 WHERE id = ?")->execute([$_SESSION['user']['id']]);
-    session_destroy();
-}
-
-echo json_encode(["success" => true]);
